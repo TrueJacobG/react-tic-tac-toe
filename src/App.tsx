c294f5355cc1ef3d3e648e2347443797f2084a06
Jakub <jakubgradzewicz1309@gmail.com>
2022-12-28 22:49:40 +0100

    simple tic tac toe without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Board from "./components/Board";
+import checkWinner from "./lib/checkWinner";
+import { symbols } from "./lib/constants";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [squares, setSquares] = useState<string[]>(Array(9).fill(" "));
+  const [count, setCount] = useState(0);
+  const [winner, setWinner] = useState(" ");
+
+  // const does not work
+  let handleClick = (key: number) => {
+    if (squares[key] !== " ") {
+      return;
+    }
+
+    squares[key] = symbols[count % 2];
+    setSquares((squares) => [...squares]);
+
+    if (checkWinner(squares)) {
+      setWinner(symbols[count % 2]);
+    }
+
+    setCount(count + 1);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Board squares={squares} handleClick={handleClick} />
+      {winner === " " ? "" : `The winner is ${winner}`}
+      {squares.every((e) => e !== " ") ? "There is no winner" : ""}
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/lib/checkWinner.ts
@@ -0,0 +1,58 @@
+export default function checkWinner(squares: string[]): boolean {
+  let i = 0;
+  while (i != 9) {
+    if (checkRow(squares, i)) return true;
+    i += 3;
+  }
+
+  i = 0;
+  while (i != 3) {
+    if (checkCol(squares, i)) return true;
+    i += 1;
+  }
+
+  checkDiagonals(squares);
+
+  return false;
+}
+
+function checkRow(squares: string[], key: number) {
+  if (
+    squares[key] !== " " &&
+    squares[key] === squares[key + 1] &&
+    squares[key + 1] === squares[key + 2]
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkCol(squares: string[], key: number) {
+  if (
+    squares[key] !== " " &&
+    squares[key] === squares[key + 3] &&
+    squares[key + 3] === squares[key + 6]
+  ) {
+    return true;
+  }
+
+  return false;
+}
+function checkDiagonals(squares: string[]) {
+  if (
+    squares[0] !== " " &&
+    squares[0] === squares[4] &&
+    squares[4] === squares[8]
+  ) {
+    return true;
+  }
+  if (
+    squares[2] !== " " &&
+    squares[2] === squares[4] &&
+    squares[4] === squares[6]
+  ) {
+    return true;
+  }
+  return false;
+}
